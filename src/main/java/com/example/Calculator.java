eff122e621e7c929d63435c8d35aa9a567619090
prashant <pbeniwal@yahoo.com>
2021-01-16 04:43:59 +0000

    updated

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -61,7 +61,7 @@ public class Calculator extends HttpServlet {
         }
     }
         public int add(int a, int b) {
-            return a + b ;
+            return a + b 
         }
         
 	    public int sub(int a, int b) {
